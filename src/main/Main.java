74fafc56bd36738c409fd48153b38d6c8962a470
lamd00 <nik.lamacz@gmail.com>
2017-10-17 10:20:28 +0200

    working UI

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,6 +14,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
@@ -29,10 +31,14 @@ import uiText.TextoveRozhrani;
  * @author xzenj02
  */
 public class Main extends Application {
+    
+    private TextArea centralText;
+    private IHra hra;
+    private TextField zadejPrikazTextArea;
 
     @Override
     public void start(Stage primaryStage) {
-        IHra hra = new Hra();
+        hra = new Hra();
         BorderPane borderPane = new BorderPane();
         
         
@@ -44,7 +50,7 @@ public class Main extends Application {
         Label zadejPrikazLabel = new Label("Zadej prikaz: ");
         zadejPrikazLabel.setFont(Font.font("Arial", FontWeight.BOLD, 14));
         
-        TextField zadejPrikazTextArea = new TextField("...");
+        zadejPrikazTextArea = new TextField("...");
         zadejPrikazTextArea.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override 
@@ -62,25 +68,38 @@ public class Main extends Application {
                         zadejPrikazTextArea.setEditable(false);
                         centralText.appendText(hra.vratEpilog());
                         
-            }             
+                                     }             
                         
                         
-                        }
+                                                }
             
-        };
-                
+              
+        });
+        
+        FlowPane obrazekFlowPane = new FlowPane();
+        obrazekFlowPane.setPrefSize(300,300);
+        
+        ImageView obrazekImageView = new ImageView(new Image(Main.class.getResourceAsStream("/zdroje/mapa.jpg"),300,300,false,true));
+          
+        obrazekFlowPane.setAlignment(Pos.CENTER);
+        obrazekFlowPane.getChildren().add(obrazekImageView);
+       
         FlowPane dolniLista = new FlowPane();
         dolniLista.setAlignment(Pos.CENTER);
         dolniLista.getChildren().addAll(zadejPrikazLabel,zadejPrikazTextArea);
+        borderPane.setLeft(obrazekFlowPane);
         borderPane.setBottom(dolniLista);
+       
         
         
-        Scene scene = new Scene(borderPane, 300, 250);
+        
+        Scene scene = new Scene(borderPane, 900, 500);
 
         primaryStage.setTitle("Adventura");
 
         primaryStage.setScene(scene);
         primaryStage.show();
+        zadejPrikazTextArea.requestFocus();
     }
 
     /**
